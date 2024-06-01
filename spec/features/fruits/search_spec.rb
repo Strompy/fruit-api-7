43652d9fcaebb434ba5dc2cc531d828032f0d2d7
Phillip Strom <phillip.strom@gmail.com>
2024-06-01 16:29:43 -0600

    Update spec to handle table

--- spec/features/fruits/search_spec.rb
+++ spec/features/fruits/search_spec.rb
@@ -6,18 +6,29 @@ RSpec.describe "/fruits", type: :feature do
             visit root_path
             fill_in 'search', with: 'Banana'
             click_on 'Search'
-            
+
             expect(current_path).to eq(fruits_path)
             expect(page).to have_content('Name: Banana')
             expect(page).to have_content('Family: Musaceae')
             expect(page).to have_content('Order: Zingiberales')
             expect(page).to have_content('Genus: Musa')
             expect(page).to have_content('Nutrition value:')
-            expect(page).to have_content('Calories: 96')
-            expect(page).to have_content('Fat: 0.2')
-            expect(page).to have_content('Sugar: 17.2')
-            expect(page).to have_content('Carbohydrates: 22')
-            expect(page).to have_content('Protein: 1')
+
+            expect(page).to have_content('Calories')
+            expect(page).to have_content('96')
+
+            expect(page).to have_content('Fat')
+            expect(page).to have_content('0.2')
+
+            expect(page).to have_content('Sugar')
+            expect(page).to have_content('17.2')
+
+            expect(page).to have_content('Carbohydrates')
+            expect(page).to have_content('22')
+
+            expect(page).to have_content('Protein')
+            expect(page).to have_content('1')
+
         end
     end
 end
