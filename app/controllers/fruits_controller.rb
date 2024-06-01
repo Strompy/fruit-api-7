229131affe1722bbe50b8be76ae2e705365447fe
Phillip Strom <phillip.strom@gmail.com>
2024-06-01 16:30:08 -0600

    Display fruit response

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -4,7 +4,12 @@ class FruitsController < ApplicationController
     end
 
     def create
-        @fruit = {}
+        # Call the service
+        @fruit = FruityViceService.search_by_name(params[:search])
+        binding.pry
+        # then maybe create a fruit model
+
+
 
         render :index
     end
