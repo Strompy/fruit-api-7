812ab585ad3b206ed31e4f43cc3cc1adcf22b5f9
Phillip Strom <phillip.strom@gmail.com>
2024-06-03 15:08:06 -0600

    Add similar fruits to fruit search spec

--- spec/features/fruits/search_spec.rb
+++ spec/features/fruits/search_spec.rb
@@ -44,5 +44,39 @@ RSpec.describe "/fruits", type: :feature do
 
             expect(page).to have_content('Apple')
         end
+
+        it 'similar fruits link to fruit by name' do
+            visit root_path
+            fill_in 'search', with: 'Banana'
+            click_on 'Search'
+
+            expect(current_path).to eq(fruits_path)
+            click_on 'Find Similar Fruits by Nutrition Value'
+
+            expect(current_path).to eq(nutritional_fruits_path)
+            click_on 'Green Apple'
+
+            expect(current_path).to eq(fruits_path)
+            expect(page).to have_content('Name: Green Apple')
+            expect(page).to have_content('Family: Rosaceae')
+            expect(page).to have_content('Order: Rosales')
+            expect(page).to have_content('Genus: Malus')
+            expect(page).to have_content('Nutrition value:')
+
+            expect(page).to have_content('Calories')
+            expect(page).to have_content('21')
+
+            expect(page).to have_content('Fat')
+            expect(page).to have_content('0.1')
+
+            expect(page).to have_content('Sugar')
+            expect(page).to have_content('6.4')
+
+            expect(page).to have_content('Carbohydrates')
+            expect(page).to have_content('3.1')
+
+            expect(page).to have_content('Protein')
+            expect(page).to have_content('0.4')
+        end
     end
 end
