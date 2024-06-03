884e397756c1bb83b633bf410a622a9700ac7c86
Phillip Strom <phillip.strom@gmail.com>
2024-06-03 15:38:14 -0600

    Add search to index

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -1,14 +1,21 @@
 class FruitsController < ApplicationController
+    before_action :search_fruit, only: [:index, :create]
+
     def index; end
 
     def create
-        @fruit = FruityViceService.search_by_name(params[:search])
-
         render :index
     end
 
     def nutritional
         @fruit_name = params[:name]
         @fruits = FruityViceService.search_by_calories(params[:nutritions][:calories])
+        @fruits.delete_if { |fruit| fruit[:name] == @fruit_name }
+    end
+
+    private
+
+    def search_fruit
+        @fruit = FruityViceService.search_by_name(params[:search]) if params[:search]
     end
 end
