f2e566fd55db70a187f5f9c03e53f7d99de5a2b6
Phillip Strom <phillip.strom@gmail.com>
2024-06-03 15:01:30 -0600

    Create calorie search

--- app/services/fruity_vice_service.rb
+++ app/services/fruity_vice_service.rb
@@ -1,10 +1,18 @@
 class FruityViceService
     def self.search_by_name(query)
-        response = conn.get("api/fruit/#{query}")
-        JSON.parse(response.body, symbolize_names: true)
+        search_by_url("api/fruit/#{query}")
+    end
+
+    def self.search_by_calories(query)
+        search_by_url("api/fruit/calories?min=#{query.to_i - 5}")
     end
 
     private
+
+    def search_by_url(url)
+        response = conn.get(url)
+        JSON.parse(response.body, symbolize_names: true)
+    end
 
     def self.conn
         Faraday.new(url: "https://www.fruityvice.com/")
